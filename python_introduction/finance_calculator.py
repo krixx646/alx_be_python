229fea597f4611674e5eac8f1b1e8e2a879a6ca9
Valentine Cheluchi <krixxvalle@gmail.com>
2025-05-25 22:32:47 +0100

    correction

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,13 +1,12 @@
 # User monthly income calculator
 # finance_calculator.py
-mi = float(input("Enter your monthly income: "))
-me = float(input("Enter your monthly expenses: "))
-ms = mi - me
+
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 
 # project annual savings
-ps = ms * 12 + (ms * 12 * 0.05)  # Assuming a 5% interest rate on savings   
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)  # Assuming a 5% interest rate on savings
 
-print(f"Your monthly savings are: {ms}")
-print("Projected savings after one year, with interest, is: {:.2f}".format(ps))
-# Output: Your monthly savings are: 500.0
-# Output: Projected savings after one year, with interest, is: 6300.00
+print(f"Your monthly savings are: {monthly_savings}")
+print("Projected savings after one year, with interest, is: {:.2f}".format(projected_savings))
