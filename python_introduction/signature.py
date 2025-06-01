ddca8ed999cd8dd8af6e469f69d9859f88720b0f
Valentine Cheluchi <krixxvalle@gmail.com>
2025-06-01 12:41:39 +0100

    strange file

---
+++ python_introduction/signature.py
@@ -0,0 +1,8 @@
+# creat signature from name
+
+first_name = input("Enter your first name: ")
+second_name = input("Enter your second name: ")
+last_name = input("Enter your last name: ")
+
+print(f"Your full name is : {first_name} {second_name} {last_name}")
+print("Your signature is", first_name[0:3], second_name[0:3], last_name[0:3])
