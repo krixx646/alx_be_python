76409a47c6904645a81a7848abba34e65b3f1495
Valentine Cheluchi <krixxvalle@gmail.com>
2025-06-14 16:59:56 +0100

    try-catch example

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,17 +2,17 @@
 
 def safe_divide(numerator, denominator):
     try:
-       x = float(numerator)
-       y = float(denominator)
-    except (TypeError, ValueError) as e:
-        print(f'one or both of the input is not a float or not decimal', e)
+        x = float(numerator)
+        y = float(denominator)
+    except (TypeError, ValueError):
+        print("Error: Please enter numeric values only.")
         return
     try:
         j = x / y
     except ZeroDivisionError:
-        # Exactly match the required message:
         print("Error: Cannot divide by zero.")
         return
     return j
+
 
 
