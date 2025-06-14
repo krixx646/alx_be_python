1831fd61d55bc148ece13f94a109f27bd84d3111
Valentine Cheluchi <krixxvalle@gmail.com>
2025-06-14 11:14:46 +0100

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+# bank account class
+
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        self.account_balance -= amount
+        return True
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
